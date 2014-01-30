be92f1b1f5ba7a8b0ac3e5fbcfb4720c140eace0
iPrayag <prayag.upd@gmail.com>
2014-01-30 23:32:10 +0545

    ADD clojure conf with marmalade

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,13 @@
 ;(require 'dirtree)
 
+;;;;;;;;;;;;;;; enable interactiveDO mode;;;;;;;;;;;;;;;;;;;;;;
 (require 'ido)
     (ido-mode t)
 
+;;;;;;;;;;;;;;;; enable line numbers ;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-linum-mode t)
 
+;;;;;;;;;;;;;;; add solarized color theme;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 
 (load-theme 'solarized-dark t)
@@ -21,7 +24,38 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; fullscreen
+;;;;;;;;;;;;;;;;;;;;;;;; fullscreen ;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
    '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; packages ;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'package)
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;; added for melpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;http://clojure-doc.org/articles/tutorials/emacs.html
+;;;;;;;;;;;;;;;;;;; clojure ;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      clojure-mode
+                      clojure-test-mode
+                      cider))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
