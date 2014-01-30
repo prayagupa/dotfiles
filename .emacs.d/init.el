ec060d372781649aa157650b6cc2601c707f29dc
iPrayag <prayag.upd@gmail.com>
2014-01-30 14:38:18 +0545

    ADD ido mode enabled and maximize in startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,27 @@
+;(require 'dirtree)
+
+(require 'ido)
+    (ido-mode t)
+
+(global-linum-mode t)
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 
 (load-theme 'solarized-dark t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; fullscreen
+(custom-set-variables
+   '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
