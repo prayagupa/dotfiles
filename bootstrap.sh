5db956f47130a75926949ebfd7e62d53fc6d477c
iPrayag <prayag.upd@gmail.com>
2014-03-31 10:10:12 +0545

    ADD bootstrap for new ubuntu installtion

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+##Sensors Indicator
+##http://www.noobslab.com/2013/04/panel-indicators-collection-for-ubuntu.html
+sudo add-apt-repository ppa:noobslab/indicators
+sudo apt-get update
+sudo apt-get install -y indicator-sensors lm-sensors
