e93ed67cb64cb954d134d9bfd8eccea1a9ed4013
Prayag Upd <prayag.upd@gmail.com>
2014-04-07 17:41:57 +0545

    ADD dotfiles bootstrap

---
+++ dotfiles_bootstrap.sh
@@ -0,0 +1,10 @@
+##########################################################################
+######## https://github.com/gf3/dotfiles/blob/master/bootstrap.sh ########
+##########################################################################
+
+cp .vimrc ~/.vimrc
+cp .bash_profile ~/.bash_profile
+cp .bashrc ~/.bashrc
+cp .gitconfig ~/.gitconfig
+cp .tmux.conf ~/.tmux.conf
+
