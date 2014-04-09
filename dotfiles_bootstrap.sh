57063b6d14941b3307dd331355e498c61bdd4298
Prayag Upd <prayag.upd@gmail.com>
2014-04-09 15:45:27 +0545

    FIX typos

--- dotfiles_bootstrap.sh
+++ dotfiles_bootstrap.sh
@@ -2,20 +2,21 @@
 ######## https://github.com/gf3/dotfiles/blob/master/bootstrap.sh ########
 ##########################################################################
 
-cp .vimrc ~/.vimrc
-cp .bash_profile ~/.bash_profile
-cp .bashrc ~/.bashrc
-cp .gitconfig ~/.gitconfig
-cp .tmux.conf ~/.tmux.conf
+cp -r .vim ~/
+cp .vimrc ~/
+cp .bash_profile ~/
+cp .bashrc ~/
+cp .gitconfig ~/
+cp .tmux.conf ~/
 
 
 configureGitDiff(){
-				#sudo touch /usr/local/bin/git_diff_wrapper
-				sudo tee -a /usr/local/bin/git_diff_wrapper  >/dev/null << 'EOF'
+#sudo touch /usr/local/bin/git_diff_wrapper
+sudo tee -a /usr/local/bin/git_diff_wrapper  >/dev/null << 'EOF'
 				#!/bin/sh
 				vimdiff "$2" "$5"
-				EOF
-				sudo chmod 777 /usr/local/bin/git_diff_wrapper
+EOF
+sudo chmod 777 /usr/local/bin/git_diff_wrapper
 }
 
 configureGitDiff
