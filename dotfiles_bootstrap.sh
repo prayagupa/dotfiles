4a6c49b64c94288f2316689291fd086c14afa5b0
iPrayag <prayag.upd@gmail.com>
2014-04-17 17:21:49 +0545

    REFACTOR README regarding dotiles bootstrap

--- dotfiles_bootstrap.sh
+++ dotfiles_bootstrap.sh
@@ -3,31 +3,55 @@
 ##########################################################################
 
 ##BACKUP
+
 backup="~/backup"
-mkdir $backup
-mv ~/.vim $backup
-mv ~/.vimrc $backup
-mv ~/.bashrc $backup
-mv ~/.gitconfig $backup
-mv ~/.bash_profile $backup
+backupDotfiles(){
+ mkdir $backup
+ mv ~/.vim $backup
+ mv ~/.vimrc $backup
+ mv ~/.bashrc $backup
+ mv ~/.gitconfig $backup
+ mv ~/.bash_profile $backup
+}
 
-cp -r .vim ~/
-cp .vimrc ~/
-cp .bash_profile ~/
-cp .bashrc ~/
-cp .gitconfig ~/
-cp .tmux.conf ~/
-cp .bash_aliases ~/
-
+installDotfiles(){
+ cp -r .vim ~/
+ cp .vimrc ~/
+ cp .bash_profile ~/
+ cp .bashrc ~/
+ cp .gitconfig ~/
+ cp .tmux.conf ~/
+ cp .bash_aliases ~/
+}
 
 configureGitDiff(){
-#sudo touch /usr/local/bin/git_diff_wrapper
-sudo tee -a /usr/local/bin/git_diff_wrapper  >/dev/null << 'EOF'
+ #sudo touch /usr/local/bin/git_diff_wrapper
+ sudo tee -a /usr/local/bin/git_diff_wrapper  >/dev/null << 'EOF'
 				#!/bin/sh
 				vimdiff "$2" "$5"
 EOF
-sudo chmod 777 /usr/local/bin/git_diff_wrapper
+ sudo chmod 777 /usr/local/bin/git_diff_wrapper
 }
 
-configureGitDiff
+askForGitconfigUser(){
+   ##TODO
+}
 
+installVundleDeps(){
+  vim +BundleInstall +qall
+}
+
+init(){
+ backupDotfiles
+ installDotfiles
+ askForGitconfigUser
+ configureGitDiff
+ installVundleDeps
+ gconftool --type string --set /desktop/gnome/background/primary_color "#002b36"
+ echo "#####################################$$$$$$#######"
+ echo "#####[info] : dotfiles installed \,,/#############"
+ echo "##################################################"
+}
+
+##main
+init
