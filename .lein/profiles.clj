733d404aaf0ca912bad84a8f241c2d711d537865
iPrayag <prayag.upd@gmail.com>
2014-09-10 15:15:17 +0545

    ADD lein exec

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [
                   [lein-swank "1.4.4"]
-                  [lein-immutant "1.2.1"]]}}
+                  [lein-immutant "1.2.1"]
+                  [lein-exec "0.3.4"]]}}
